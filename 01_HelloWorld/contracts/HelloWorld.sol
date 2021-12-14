48da9d42ecab02548a98420bd48add414832b592
Amit Yadav <ay.puzzler@gmail.com>
2021-12-14 10:56:42 +0530

    contract created

---
+++ 01_HelloWorld/contracts/HelloWorld.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+/**
+ * @title HelloWorld
+ * @dev Store & retrieve value in a variable
+ */
+contract HelloWorld {
+  string message;
+
+  // initializes the state variable with a default value
+  constructor() {
+    message = "Hello World";
+  }
+
+  /**
+   * @dev Store value in variable
+   * @param _message value to store
+   */
+  function store(string memory _message) public {
+      message = _message;
+  }
+
+  /**
+   * @dev Return value 
+   * @return value of 'message'
+   */
+  function retrieve() public view returns (string memory){
+      return message;
+  }
+}
