319f05441e775700bd9032dbc6bc19ba5d376570
Amit Yadav <ay.puzzler@gmail.com>
2021-12-14 11:54:47 +0530

    vue3js app initialized using vite and tailwind css installed and configured into it

---
+++ 01_HelloWorld/vue3-frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Message from './components/Message.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300 py-6 flex flex-col justify-center relative overflow-hidden sm:py-12">
+  <div class="relative px-6 pt-10 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 sm:max-w-lg sm:mx-auto sm:rounded-lg sm:px-10">
+    <div class="max-w-md mx-auto">
+      <img src="./assets/logo.png" class="h-14 mx-auto mb-4" />
+      <Message msg="Hello Vue 3 + Vite" />
+    </div>
+  </div>
+</div>
+</template>
---
+++ 01_HelloWorld/vue3-frontend/src/components/Message.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold text-gray-600 mx-auto">
+    {{ msg }}
+  </h1>
+</template>
