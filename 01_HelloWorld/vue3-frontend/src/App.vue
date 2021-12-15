53c9ca5d9363ac6d7e626a3479c6611e6a15c7b2
Amit Yadav <ay.puzzler@gmail.com>
2021-12-15 13:11:01 +0530

    store created using vue3 reactivity

--- 01_HelloWorld/vue3-frontend/src/App.vue
+++ 01_HelloWorld/vue3-frontend/src/App.vue
@@ -12,6 +12,7 @@ export default {
   },
   setup() {
     provide('store', store)
+    store.methods.connectBlockchain()
   }
 }
 </script>
--- 01_HelloWorld/vue3-frontend/src/store/index.js
+++ 01_HelloWorld/vue3-frontend/src/store/index.js
@@ -1,12 +1,28 @@
 import { reactive } from 'vue'
+import Web3 from 'web3'
 
 const state = reactive({
-    message: 'Hello World'
+    message: 'Hello World',
+    error: '',
+    isLoading: false
 })
 
 const methods = {
     connectBlockchain() {
-        console.log("connected from store")
+        // console.log('connect blockchain')
+        const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
+        console.log(web3)
+        // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+        // if (typeof web3 !== 'undefined') {
+        //     // Use Mist/MetaMask's provider
+        //     web3js = new Web3(web3.currentProvider)
+        //     console.log("connected...")
+        // } else {
+        //     // Handle the case where the user doesn't have web3. Probably
+        //     // show them a message telling them to install Metamask in
+        //     // order to use our app.
+        //     console.log("Web3 Provide not found, install Crypto wallet like Metamask to use ur dapp")
+        // }
     },
     retrieveMessage() {
         state.message = 'New Hello'
