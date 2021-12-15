c2cd614d52bd04a53a32a3e2582d1e4fd4a67e97
Amit Yadav <ay.puzzler@gmail.com>
2021-12-15 13:14:58 +0530

    vite with web3 specific dependencies added

--- 01_HelloWorld/vue3-frontend/vite.config.js
+++ 01_HelloWorld/vue3-frontend/vite.config.js
@@ -3,5 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      process: "process/browser",
+      stream: "stream-browserify",
+      zlib: "browserify-zlib",
+      util: 'util'
+    }
+  },
   plugins: [vue()]
 })
