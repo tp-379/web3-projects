878f16b40db867dff6873432cf9aca1c70a8c348
Amit Yadav <ay.puzzler@gmail.com>
2021-12-16 12:22:22 +0530

    interacted with ganache

--- 01_HelloWorld/vue3-frontend/src/App.vue
+++ 01_HelloWorld/vue3-frontend/src/App.vue
@@ -13,6 +13,11 @@ export default {
   setup() {
     provide('store', store)
     store.methods.connectBlockchain()
+    store.methods.getContractData()
+
+    return {
+      store
+    }
   }
 }
 </script>
@@ -20,8 +25,12 @@ export default {
 <template>
   <div class="min-h-screen bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300 py-6 flex flex-col justify-center relative overflow-hidden sm:py-12">
   <div class="relative px-6 pt-10 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 sm:max-w-lg sm:mx-auto sm:rounded-lg sm:px-10">
-    <div class="max-w-md mx-auto">
+    <div class="max-w-md mx-auto" v-if="store.state.isLoading">
+      <div class="font-bold text-purple-500 mx-auto">loading...</div>
+    </div>
+    <div class="max-w-md mx-auto" v-else>
       <img src="./assets/logo.png" class="h-18 mx-auto mb-4" />
+      <div class="font-bold text-red-500 mx-auto">{{ store.state.error }}</div>
       <retrieve-message class="text-center mb-8"/>
       <store-message />
     </div>
--- 01_HelloWorld/vue3-frontend/src/store/index.js
+++ 01_HelloWorld/vue3-frontend/src/store/index.js
@@ -1,36 +1,57 @@
 import { reactive } from 'vue'
 import Web3 from 'web3'
+import HelloWorld from './../build/contracts/HelloWorld.json'
 
 const state = reactive({
-    message: 'Hello World',
+    message: '',
+    contractData: '',
+    defaultAccount: '',
     error: '',
     isLoading: false
 })
 
 const methods = {
-    connectBlockchain() {
-        // console.log('connect blockchain')
-        const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
-        console.log(web3)
-        // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-        // if (typeof web3 !== 'undefined') {
-        //     // Use Mist/MetaMask's provider
-        //     web3js = new Web3(web3.currentProvider)
-        //     console.log("connected...")
-        // } else {
-        //     // Handle the case where the user doesn't have web3. Probably
-        //     // show them a message telling them to install Metamask in
-        //     // order to use our app.
-        //     console.log("Web3 Provide not found, install Crypto wallet like Metamask to use ur dapp")
-        // }
+    async connectBlockchain() {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+          }
+          else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+          }
+          else {
+           console.log('Need to install MetaMask!')
+          }
     },
-    retrieveMessage() {
-        state.message = 'New Hello'
+    async getContractData() {
+        state.isLoading = true
+        const web3js = window.web3
+        const getAccounts = await web3.eth.getAccounts()
+        // console.log(getAccounts)
+        state.defaultAccount = getAccounts[0]
+        const networkId = await web3js.eth.net.getId()
+        const networkData = HelloWorld.networks[networkId]
+        if(networkData) {
+            const helloWorld = new web3.eth.Contract(HelloWorld.abi, networkData.address)
+            state.contractData = helloWorld
+            const message = await helloWorld.methods.retrieve().call()
+            // const message = await helloWorld.methods.message().call()
+            state.message = message
+            state.isLoading = false
+          } else {
+            state.error = 'contract not deployed to detected network.'
+            state.isLoading = false
+          }
     },
     storeMessage(message) {
-        console.log('storing value: ', message)
+        state.loading = true
+        state.contractData.methods.store(message).send({ from: state.defaultAccount })
+        .once('receipt', (receipt) => {
+            console.log(receipt)
+            state.loading = false
+            window.location.reload()
+        })
     }
-
 }
 
 export default {
