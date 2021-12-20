842de5f04f1f8a109b55ad9fad217de2ed7732fc
Amit Yadav <ay.puzzler@gmail.com>
2021-12-20 09:04:11 +0530

    withdraw() function addded to contract, so that anyone can withdraw some amount of ether(0.1 ETH in our case) from the contract

--- 02_Faucet/contracts/Faucet.sol
+++ 02_Faucet/contracts/Faucet.sol
@@ -5,4 +5,13 @@ contract Faucet {
 
   // Accept any incoming amount
   receive() external payable {}
+
+  // Give out ether to anyone who asks
+  function withdraw(uint withdraw_amount) public {
+      // Limit withdrawal amount
+      require(withdraw_amount <= 100000000000000000);
+
+      // Send the amount to the address that requested it
+      msg.sender.transfer(withdraw_amount);
+  }
 }
