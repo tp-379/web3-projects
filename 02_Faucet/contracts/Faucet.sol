70b567b8e5329a96febebe7ef0c05d4fe0c3f7ed
Amit Yadav <ay.puzzler@gmail.com>
2021-12-20 09:49:26 +0530

    added inheritence, created base contract Owned and Mortal(it inherits Owned) which is inherited by Faucet. This is done to achieve modularity, extensibility and reuse

--- 02_Faucet/contracts/Faucet.sol
+++ 02_Faucet/contracts/Faucet.sol
@@ -1,33 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Faucet {
+contract Owned {
+    address payable owner;
 
-  address payable owner;
+    // Contract constructor: set owner
+    constructor() {
+        owner = msg.sender;
+    }
 
-  // Initialize Faucet contract: set owner
-  constructor() {
-    owner = msg.sender;
-  }
+    // Access control modifier
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+}
 
-  // Access control modifier
-  modifier onlyOwner {
-      require(msg.sender == owner);
-      _;
-  }
+contract Mortal is Owned {
+    // Contract destructor
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+}
+
+contract Faucet is Mortal {
 
   // Accept any incoming amount
   receive() external payable {}
 
-  // Contract destructor
-  function destroy() public onlyOwner {
-      selfdestruct(owner);
-  }
-
   // Give out ether to anyone who asks
   function withdraw(uint withdraw_amount) public {
       // Limit withdrawal amount
-      require(withdraw_amount <= 100000000000000000);
+      require(withdraw_amount <= 0.1 ether);
 
       // Send the amount to the address that requested it
       msg.sender.transfer(withdraw_amount);
