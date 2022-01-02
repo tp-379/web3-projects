7b273bebe1a80671f1b4fa49a7b4843ecceb62cf
Amit Yadav <ay.puzzler@gmail.com>
2022-01-02 09:56:49 +0530

    store and components created

--- 02_Faucet/vue3-frontend/src/App.vue
+++ 02_Faucet/vue3-frontend/src/App.vue
@@ -1,10 +1,23 @@
 <script>
+import { provide } from "vue"
+import store from "./store"
 import ConnectWallet from "./components/ConnectWallet.vue"
+import ContractData from "./components/ConractData.vue"
+import WithdrawForm from "./components/WithdrawEtherForm.vue"
 
 export default {
   name: "App",
   components: {
     ConnectWallet,
+    ContractData,
+    WithdrawForm,
+  },
+  setup() {
+    provide("store", store)
+
+    return {
+      store,
+    }
   },
 }
 </script>
@@ -59,73 +72,14 @@ export default {
         <div class="divide-y divide-gray-300/50">
           <div class="py-8 text-base leading-7 space-y-6 text-gray-600">
             <connect-wallet />
-            <ul class="space-y-4">
-              <li class="flex items-center">
-                <svg
-                  class="w-6 h-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <circle cx="12" cy="12" r="11" />
-                  <path
-                    d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
-                    fill="none"
-                  />
-                </svg>
-                <p class="ml-4">
-                  Customizing your
-                  <code class="text-sm font-bold text-gray-900"
-                    >tailwind.config.js</code
-                  >
-                  file
-                </p>
-              </li>
-              <li class="flex items-center">
-                <svg
-                  class="w-6 h-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <circle cx="12" cy="12" r="11" />
-                  <path
-                    d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
-                    fill="none"
-                  />
-                </svg>
-                <p class="ml-4">
-                  Extracting classes with
-                  <code class="text-sm font-bold text-gray-900">@apply</code>
-                </p>
-              </li>
-              <li class="flex items-center">
-                <svg
-                  class="w-6 h-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <circle cx="12" cy="12" r="11" />
-                  <path
-                    d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
-                    fill="none"
-                  />
-                </svg>
-                <p class="ml-4">Code completion with instant preview</p>
-              </li>
-            </ul>
-            <p>
-              Perfect for learning how the framework works, prototyping a new
-              idea, or creating a demo to share online.
-            </p>
+            <contract-data />
           </div>
-          <div class="pt-8 text-base leading-7 font-semibold">
-            <p class="text-gray-900">Want to dig deeper into Tailwind?</p>
-            <p>
-              <a
-                href="https://tailwindcss.com/docs"
-                class="text-sky-500 hover:text-sky-600"
-                >Read the docs &rarr;</a
-              >
+          <div class="pt-8 text-base leading-7">
+            <p class="mb-2">
+              Want to withdraw ETHER? You can withdraw only upto 0.5 ETH at a
+              time.
             </p>
+            <withdraw-form />
           </div>
         </div>
       </div>
