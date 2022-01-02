b1436ef474523cf38be4bee2ad02b522d8a80fb9
Amit Yadav <ay.puzzler@gmail.com>
2022-01-02 10:00:54 +0530

    inatalled some dependencies and changed vite.config to avoid process is not defined error

--- 02_Faucet/vue3-frontend/vite.config.js
+++ 02_Faucet/vue3-frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+export default {
+  resolve: {
+    alias: {
+      process: "process/browser",
+      stream: "stream-browserify",
+      zlib: "browserify-zlib",
+      util: "util",
+    },
+  },
+  plugins: [vue()],
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+// export default defineConfig({
+//   plugins: [vue()]
+// })
