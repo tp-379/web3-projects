443a9284c954133b03501358450548fd1e98d57e
Amit Yadav <ay.puzzler@gmail.com>
2022-01-06 13:43:48 +0530

    finalized contract

--- 03_CrowdFunding/contracts/CrowdFunding.sol
+++ 03_CrowdFunding/contracts/CrowdFunding.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+contract CampaignFactory {
+    CrowdFunding[] public deployedCampaigns;
+
+    function createCampaign(uint256 minimum) public {
+        CrowdFunding newCampaign = new CrowdFunding(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns()
+        public
+        view
+        returns (CrowdFunding[] memory)
+    {
+        return deployedCampaigns;
+    }
+}
+
 /**
  * @title CrowdFunding
  * @dev CrowdFunding is a contract that allows multiple parties to contribute to a single project.
@@ -10,12 +27,15 @@ contract CrowdFunding {
     struct Request {
         string description; // description of request
         uint256 value; // amount requested in request
-        address recipient; // address of vendor/recipient whom money will be sent
+        address payable recipient; // address of vendor/recipient whom money will be sent
         bool complete; // True if request is completed
+        uint256 approvalCount; // number of approvals
+        mapping(address => bool) approvals; // mapping of address to bool indicating if they approved
     }
 
-    // array of requests
-    Request[] public requests;
+    uint256 numRequests;
+
+    mapping(uint256 => Request) public requests;
 
     // state variable of type address containing manager's address
     address public manager;
@@ -23,8 +43,11 @@ contract CrowdFunding {
     //state variable of type uint contating minimum contribution amount
     uint256 public minContribution;
 
-    // state variable of type array of addresses containing addresses of approvers
-    address[] public approvers;
+    // address to bool mapping for approvers
+    mapping(address => bool) public approvers;
+
+    // state variable to store number of approvers
+    uint256 public approversCount;
 
     // modifier onlyByManager is a modifier that checks if the sender is the manager
     modifier onlyByManager() {
@@ -36,8 +59,8 @@ contract CrowdFunding {
     }
 
     // Contract constructor: set manager, set minimum contribution
-    constructor(uint256 _minContribution) {
-        manager = msg.sender;
+    constructor(uint256 _minContribution, address _manager) {
+        manager = _manager;
         minContribution = _minContribution;
     }
 
@@ -49,23 +72,78 @@ contract CrowdFunding {
             "Contibution must be greater than or equal to minimum value"
         );
 
-        // push the address of the contributor in approvers array
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+        approversCount++;
     }
 
     // function to create a request
     function createRequest(
         string memory _description,
         uint256 _value,
-        address _recipient
+        address payable _recipient
     ) public onlyByManager {
-        Request memory newRequest = Request({
-            description: _description,
-            value: _value,
-            recipient: _recipient,
-            complete: false
-        });
+        Request storage newRequest = requests[numRequests++];
+        newRequest.description = _description;
+        newRequest.value = _value;
+        newRequest.recipient = _recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+    }
 
-        requests.push(newRequest);
+    // function to approve a request
+    function approveRequest(uint256 _requestIndex) public {
+        Request storage request = requests[_requestIndex];
+
+        require(
+            approvers[msg.sender],
+            "Only contributors can approve requests."
+        );
+
+        require(
+            !request.approvals[msg.sender],
+            "You have already voted for this request"
+        );
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    // function to finalize a request
+    function finalizeRequest(uint256 _requestIndex) public onlyByManager {
+        Request storage request = requests[_requestIndex];
+
+        require(
+            request.approvalCount > (approversCount / 2),
+            "More approvals needed to finalize request"
+        );
+
+        require(!(request.complete), "Request is already finalized");
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+
+    function getSummary()
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            address
+        )
+    {
+        return (
+            minContribution,
+            address(this).balance,
+            numRequests,
+            approversCount,
+            manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint256) {
+        return numRequests;
     }
 }
