f1c26fbb56e9ec830f7f5eacbbdb14f5e8c98b6f
Amit Yadav <ay.puzzler@gmail.com>
2022-01-06 11:18:12 +0530

    added a contribute function and an array of approvers

--- 03_CrowdFunding/contracts/CrowdFunding.sol
+++ 03_CrowdFunding/contracts/CrowdFunding.sol
@@ -12,9 +12,23 @@ contract CrowdFunding {
     //state variable of type uint contating minimum contribution amount
     uint256 public minContribution;
 
+    // state variable of type array of addresses containing addresses of approvers
+    address[] public approvers;
+
     // Contract constructor: set manager, set minimum contribution
     constructor(uint256 _minContribution) {
         manager = msg.sender;
         minContribution = _minContribution;
+    }
+
+    function contribute() public payable {
+        // checking if the incoming contribution amount is greater than or equal to minimum contribution
+        require(
+            msg.value >= minContribution,
+            "Contibution must be greater than or equal to minimum value"
+        );
+
+        // push the address of the contributor in approvers array
+        approvers.push(msg.sender);
     }
 }
