c6dcd3ea31c30d1b0dad139e6e7ca9d3c5773e75
Amit Yadav <ay.puzzler@gmail.com>
2022-01-06 11:07:57 +0530

    created state variables and a constructor to initialize them

--- 03_CrowdFunding/contracts/CrowdFunding.sol
+++ 03_CrowdFunding/contracts/CrowdFunding.sol
@@ -1,7 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+/**
+ * @title CrowdFunding
+ * @dev
+ */
 contract CrowdFunding {
-  constructor() public {
-  }
+    // state variable of type address containing manager's address
+    address public manager;
+
+    //state variable of type uint contating minimum contribution amount
+    uint256 public minContribution;
+
+    // Contract constructor: set manager, set minimum contribution
+    constructor(uint256 _minContribution) {
+        manager = msg.sender;
+        minContribution = _minContribution;
+    }
 }
