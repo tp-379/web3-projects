4a659353e7a38f637f7d0f50b94c6e84e86d37c2
Amit Yadav <ay.puzzler@gmail.com>
2022-01-30 16:16:50 +0530

    my dapp-starter cloned

---
+++ 03_CrowdFunding/vue3-frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="min-h-full">
+    <banner
+      v-if="$store.state.error"
+      class="bg-rose-600"
+      :message="$store.state.error"
+    />
+    <nav-bar />
+    <main>
+      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <router-view />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+import Banner from './components/Banner.vue'
+import NavBar from './components/NavBar.vue'
+
+export default {
+  components: {
+    Banner,
+    NavBar,
+  },
+  setup() {
+    const store = useStore()
+    store.dispatch('isMetaMaskInstalled')
+  },
+}
+</script>
---
+++ 03_CrowdFunding/vue3-frontend/src/store/index.js
@@ -0,0 +1,91 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    isMetaMask: false,
+    defaultAccount: '',
+    networkId: '',
+    error: '',
+    isLoading: false,
+  },
+  mutations: {
+    setLoading(state, payload) {
+      state.isLoading = payload
+    },
+    setError(state, payload) {
+      state.error = payload
+    },
+    clearError(state) {
+      state.error = null
+    },
+    setIsMetaMask(state, payload) {
+      state.isMetaMask = payload
+    },
+    setDefaultAccount(state, payload) {
+      state.defaultAccount = payload
+    },
+    setNetworkId(state, payload) {
+      state.networkId = payload
+    },
+  },
+  actions: {
+    isMetaMaskInstalled({ commit }) {
+      let provider = window.ethereum
+      if (typeof provider !== 'undefined') {
+        commit('setIsMetaMask', provider.isMetaMask)
+        commit('setNetworkId', provider.networkVersion)
+      } else {
+        commit(
+          'setError',
+          'Please install MetaMask to interact with this DApp!'
+        )
+      }
+    },
+    async onClickConnect({ commit }) {
+      let provider = window.ethereum
+      try {
+        commit('setLoading', true)
+        commit('clearError')
+        const accounts = await provider
+          .request({
+            method: 'wallet_requestPermissions',
+            params: [
+              {
+                eth_accounts: {},
+              },
+            ],
+          })
+          .then(() =>
+            ethereum.request({
+              method: 'eth_requestAccounts',
+            })
+          )
+        commit('setDefaultAccount', accounts[0])
+        commit('setLoading', false)
+      } catch (err) {
+        commit('setError', err.message)
+      }
+    },
+    clearError({ commit }) {
+      commit('clearError')
+    },
+  },
+  getters: {
+    getMetaMaskState(state) {
+      return state.isMetaMask
+    },
+    getDefaultAccount(state) {
+      return state.defaultAccount
+    },
+    getNetworkId(state) {
+      return state.networkId
+    },
+    getLoading(state) {
+      return state.isLoading
+    },
+    getError(state) {
+      return state.error
+    },
+  },
+  modules: {},
+})
