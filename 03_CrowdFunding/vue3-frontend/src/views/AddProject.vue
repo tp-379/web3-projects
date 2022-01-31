8054afa4d9a27c54e2450319724fcd266a6c6f5e
Amit Yadav <ay.puzzler@gmail.com>
2022-01-31 11:23:41 +0530

    New page to add new project added

---
+++ 03_CrowdFunding/vue3-frontend/src/views/AddProject.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="px-4 py-6 sm:px-0">
+    <AddProjectForm />
+  </div>
+</template>
+<script>
+import AddProjectForm from './../components/AddProjectForm.vue'
+
+export default {
+  components: {
+    AddProjectForm,
+  },
+}
+</script>
