87b822a2dfcd652df421da0e6bddd6d7e0e7fe7a
Amit Yadav <ay.puzzler@gmail.com>
2022-02-05 11:11:49 +0530

    resolved errors of vitejs with web3, process is not defined

--- 03_CrowdFunding/vue3-frontend/vite.config.js
+++ 03_CrowdFunding/vue3-frontend/vite.config.js
@@ -3,5 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      process: 'process/browser',
+      // stream: 'stream-browserify',
+      zlib: 'browserify-zlib',
+      util: 'util',
+    },
+  },
+  plugins: [vue()],
 })
