8a66b55bdbe82ffd328f8084433618a25003ebe3
Amit Yadav <ay.puzzler@gmail.com>
2022-02-05 09:12:07 +0530

    added rinkeby network settings

--- 03_CrowdFunding/truffle-config.js
+++ 03_CrowdFunding/truffle-config.js
@@ -18,10 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const fs = require('fs')
+const mnemonic = fs.readFileSync('.secret').toString().trim()
+const infuraURL =
+  'https://rinkeby.infura.io/v3/8dea05a4df56412eb7635af9c6810ff5'
 
 module.exports = {
   /**
@@ -58,6 +59,14 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infuraURL),
+      network_id: 4, // Rinkeby's id
+      gas: 5500000, // Rinkeby has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     // ropsten: {
     // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
     // network_id: 3,       // Ropsten's id
