6ea678aa1b1482245c11b2b39290355b6a77d032
Amit Yadav <ay.puzzler@gmail.com>
2022-02-07 09:41:26 +0530

    tests written and verified, except 1 test to finalize a request is failing

--- 03_CrowdFunding/test/crowdFunding.test.js
+++ 03_CrowdFunding/test/crowdFunding.test.js
@@ -1,45 +1,114 @@
 const CrowdFunding = artifacts.require('CrowdFunding')
 const ProjectFactory = artifacts.require('ProjectFactory')
+// const compiledProjectFactory = require('../../build/contracts/ProjectFactory.json')
+// const compiledCrowdFunding = require('../../build/contracts/CrowdFunding.json')
 
 contract('CrowdFunding', () => {
   let crowdFunding
   let projectFactory
+  let project
   let accounts
 
-  before(async () => {
+  beforeEach(async () => {
     crowdFunding = await CrowdFunding.deployed()
     projectFactory = await ProjectFactory.deployed()
     accounts = await web3.eth.getAccounts()
+
+    await projectFactory.createProject('1500', { from: accounts[0] })
+
+    deployedProjects = await projectFactory.getDeployedProjects()
+    project = await new web3.eth.Contract(crowdFunding.abi, deployedProjects[0])
   })
 
-  it('project factory gets deployed', async () => {
-    const factoryAddress = projectFactory.address
-    assert.notEqual(factoryAddress, 0x0)
-    assert.notEqual(factoryAddress, '')
-    assert.notEqual(factoryAddress, null)
-    assert.notEqual(factoryAddress, undefined)
-  })
-
-  it('crowd funding gets deployed', async () => {
-    const crowdFundingAddress = crowdFunding.address
-    assert.notEqual(crowdFundingAddress, 0x0)
-    assert.notEqual(crowdFundingAddress, '')
-    assert.notEqual(crowdFundingAddress, null)
-    assert.notEqual(crowdFundingAddress, undefined)
-  })
-
-  it('has a manager', async () => {
-    const manager = await crowdFunding.manager()
-    // console.log(manager)
-    assert.notEqual(manager, 0x0)
-    assert.notEqual(manager, '')
-    assert.notEqual(manager, null)
-    assert.notEqual(manager, undefined)
+  it('deploys project factory and crowdfunding', async () => {
+    assert.ok(projectFactory.address)
+    assert.ok(project.options.address)
   })
 
   it('sets deployer as manager', async () => {
     const deployer = accounts[0]
-    const manager = await crowdFunding.manager()
+    const manager = await project.methods.manager().call()
     assert.equal(deployer, manager)
+  })
+
+  // it('requires a min contribution', async () => {
+  //   await project.methods.contribute().send({
+  //     value: '5',
+  //     from: accounts[1],
+  //   })
+  //   assert(false)
+  // })
+  it('requires a min contribution', async () => {
+    try {
+      await project.methods.contribute().send({
+        value: '5',
+        from: accounts[1],
+      })
+      assert(false)
+    } catch (err) {
+      assert(err)
+    }
+  })
+
+  it('allows people to contribute and makes them approver', async () => {
+    await project.methods.contribute().send({
+      from: accounts[1],
+      gas: '1500000',
+      value: '2000',
+    })
+
+    const isContributor = await project.methods.approvers(accounts[1]).call()
+    assert.equal(isContributor, true)
+  })
+
+  it('allows a manager to make a payment request', async () => {
+    await project.methods
+      .createRequest('Buy Sensors', '5000', accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1500000',
+      })
+    const request = await project.methods.requests(0).call()
+    assert.equal('Buy Sensors', request.description)
+  })
+
+  // Not working properly, shows VM Exception while processing transaction: revert More approvals needed to finalize request
+  // Will come back to it later
+  it('processes requests', async () => {
+    // Let accounts[1] contribute 5 ether to our project.
+    await project.methods.contribute().send({
+      value: web3.utils.toWei('5', 'ether'),
+      from: accounts[1],
+    })
+
+    // Create a spend request for 2 ether to go to accounts[2].
+    await project.methods
+      .createRequest(
+        'A new spend request',
+        web3.utils.toWei('2', 'ether'),
+        accounts[2]
+      )
+      .send({
+        from: accounts[0],
+        gas: '1500000',
+      })
+
+    // Approve the spend request.
+    await project.methods.approveRequest(0).send({
+      from: accounts[1],
+      gas: '1500000',
+    })
+
+    // Finalize the request.
+    await project.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: '1500000',
+    })
+
+    let balance = await web3.eth.getBalance(accounts[2])
+    balance = web3.utils.fromWei(balance, 'ether')
+    balance = parseFloat(balance)
+
+    assert(balance > 101)
   })
 })
