b9e4227547ea26a8bfefff3a9cf56fa355da67ac
Amit Yadav <ay.puzzler@gmail.com>
2022-03-16 11:42:23 +0530

    updated truffle config to deploy to rinkeby

--- 03_Lottery/truffle-config.js
+++ 03_Lottery/truffle-config.js
@@ -18,10 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const fs = require('fs')
+const mnemonic = fs.readFileSync('.secret').toString().trim()
+const infuraURL =
+  'https://rinkeby.infura.io/v3/65c89fe41a7c451f90ab0f7949e87456'
 
 module.exports = {
   /**
@@ -33,6 +34,7 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
+  contracts_build_directory: './react-frontend/src/build/contracts/',
 
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
@@ -57,6 +59,14 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infuraURL),
+      network_id: 4, // Rinkeby's id
+      gas: 5500000, // Rinkeby has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     // ropsten: {
     // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
     // network_id: 3,       // Ropsten's id
