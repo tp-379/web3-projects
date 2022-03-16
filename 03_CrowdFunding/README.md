4d5626a4a5d108bb4c366466ce91363867b861f5
Amit Yadav <ay.puzzler@gmail.com>
2022-03-16 07:17:27 +0530

    removed crowdfunding project

--- 03_CrowdFunding/README.md
+++
@@ -1,6 +0,0 @@
-<h1 align="center"> 
-03 - Crowd Funding Project
-</h1>
-<h3 align="center">
-Built using <b>Truffle Framework</b> and <b>Vuejs3</b>
-</h3>
--- 03_CrowdFunding/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const CrowdFunding = artifacts.require('CrowdFunding')
-const ProjectFactory = artifacts.require('ProjectFactory')
-
-module.exports = function (deployer, network, accounts) {
-  deployer.deploy(ProjectFactory)
-  deployer.deploy(CrowdFunding, web3.utils.toWei('1', 'ether'), accounts[0])
-}
